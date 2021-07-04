8fa0bbc1be81ff9ce44eb55e30d73a41a4c326ab
aiki miyajima <82563973+Aiki-Miyajima@users.noreply.github.com>
2021-07-04 13:18:15 +0000

    I finished Exercise10_Problem1_3.

--- Exercise_10_Problem_1_3.py
+++ Exercise_10_Problem_1_3.py
@@ -128,9 +128,12 @@ print(pop.head(3))
 
 # Create a spatial join between grid layer and buffer layer. 
 # YOUR CDOE HERE 10 for spatial join
-
+join = gpd.sjoin(geodata, pop, how="inner", op="intersects")
 
 # YOUR CODE HERE 11 to report how many people live within 1.5 km distance from each shopping center
+grouped = join.groupby('name')
+for key, group in grouped:
+    print('store: ', key,"\n", 'population:', sum(group['residents']))
 
 # **Reflections:**
 #     
@@ -139,5 +142,7 @@ print(pop.head(3))
 # - What was difficult?
 
 # YOUR ANSWER HERE
+# - All of them was not easy.
+# - Problem3 was difficult.
 
 # Well done!

dc45f3535655f183bdf847b632ca2fe81b472efa
aiki miyajima <82563973+Aiki-Miyajima@users.noreply.github.com>
2021-07-04 13:07:55 +0000

    I finished Exercise10.

--- Exercise_10_Problem_1_3.py
+++ Exercise_10_Problem_1_3.py
@@ -12,6 +12,10 @@ import pandas as pd
 # Read the data (replace "None" with your own code)
 data = None
 # YOUR CODE HERE 1 to read the data
+data = pd.read_table('shopping_centers.txt', sep=';', header=None)
+data.index.name = 'id'
+data.columns=['name', 'addr']
+
 
 #TEST COEE
 # Check your input data
@@ -25,6 +29,7 @@ from geopandas.tools import geocode
 
 # Geocode addresses using Nominatim. Remember to provide a custom "application name" in the user_agent parameter!
 #YOUR CODE HERE 2 for geocoding
+geo = geocode(data['addr'], provider='nominatim', user_agent='application name')
 
 #TEST CODE
 # Check the geocoded output
@@ -38,6 +43,9 @@ print(type(geo))
 # Check that the coordinate reference system of the geocoded result is correctly defined, and **reproject the layer into JGD2011** (EPSG:6668):
 
 # YOUR CODE HERE 3 to set crs.
+from pyproj import CRS
+geo = geo.to_crs(CRS.from_epsg(6668))
+
 
 #TEST CODE
 # Check layer crs
@@ -45,7 +53,7 @@ print(geo.crs)
 
 
 # YOUR CODE HERE 4 to join the tables
-geodata = None
+geodata = geo.join(data)
 
 #TEST CODE
 # Check the join output
@@ -57,7 +65,8 @@ print(geodata.head())
 # Define output filepath
 out_fp = None
 # YOUR CODE HERE 5 to save the output
-
+out_fp = r"shopping_centers.shp"
+geodata.to_file(out_fp)
 # TEST CODE
 # Print info about output file
 print("Geocoded output is stored in this file:", out_fp)
@@ -69,9 +78,9 @@ print("Geocoded output is stored in this file:", out_fp)
  
 
 # YOUR CODE HERE 6 to create a new column
-
+geodata['buffer']=None
 # YOUR CODE HERE 7 to set buffer column
-
+geodata['buffer'] = geodata['geometry'].buffer(distance=1500)
 #TEST CODE
 print(geodata.head())
 
@@ -88,7 +97,7 @@ print(round(gpd.GeoSeries(geodata["buffer"]).area / 1000000))
 # - Replace the values in `geometry` column with the values of `buffer` column:
 
 # YOUR CODE HERE 8 to replace the values in geometry
-
+geodata['geometry'] = geodata['buffer']
 #TEST CODE
 print(geodata.head())
 
@@ -100,6 +109,13 @@ print(geodata.head())
 
 # YOUR CODE HERE 9
 # Read population grid data for 2018 into a variable `pop`. 
+import geojson
+import requests
+url = 'https://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-mesh500h30.html#prefecture13'
+params = dict(service='WFS',version='2.0.0',request='GetFeature',
+              typeName='asuminen_ja_maankaytto:Vaestotietoruudukko_2018',outputFormat='geojson')
+r = requests.get(url, params=params)
+pop = gpd.GeoDataFrame.from_features(geojson.loads(r.content))
 
 #TEST CODE
 # Check your input data
